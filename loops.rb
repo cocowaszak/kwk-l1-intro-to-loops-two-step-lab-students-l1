bef97d19a5c0d9e7d2654d5cc836bf696f3df298
cocowaszak <colettewaszak@gmail.com>
2018-06-06 22:35:54 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,57 @@
+#loop do 
+ # puts "To the right, to the right"
+#  puts "To the left, to the left"
+ # puts "Now kick, kick"
+#  puts "Now walk it out"
+#break
+#end 
+
+#def dance
+  
+ # moves_completed = 0
+  
+ # loop do
+  #  if moves_completed == 10
+  #    break 
+  #  end #ends if statement 
+    
+ # puts "To the left, to the left"
+ # puts "Now walk it out"
+ 
+# moves_completed += 1
+# puts "#{moves_completed} move(s) completed!"
+# end #ends the loop
+ 
+# end #ends the method
+ 
+# dance 
+
+
+
+
+
+
+
+
+
+
+
+
+
+def dance 
+  moves_completed = 0
+  
+  while moves_completed < 10
+  puts "To the left, to the left"
+  puts "Walk it out"
+  
+  moves_completed +=1
+  #moves_completed = moves_completed + 1
+  
+  puts "#{moves_completed} move(s) completed!"
+  
+end #end while
+
+end #end method
+
+dance
